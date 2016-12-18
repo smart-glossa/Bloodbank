e0311db711280c53a0777928716bd5cd558a4488
Sathish <sathishprs@smartglossa>
2016-12-18 10:25:33 +0530

    Add Servlet file

---
+++ bloodBank/src/main/java/com/smartglossa/BloodBank/BloodServlet.java
@@ -0,0 +1,26 @@
+package com.smartglossa.BloodBank;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class BloodServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		
+
+	}
+}
